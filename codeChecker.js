0c115da07fe3da99a4e379b040e45886b34403bf
Kay <kard@hotmail.co.uk>
2023-03-27 17:17:39 +0100

    Returns no if there are letters in the translations that are not in the letter pairs

--- codeChecker.js
+++ codeChecker.js
@@ -1,5 +1,11 @@
-const codeChecker = (translations) => {
-  return translations[3] == "aa bb" ? ["yes"] : ["no"]
+const codeChecker = (input) => {
+  // create array of letters in letter pairs
+  const letterPairs = input[2].split("");
+  // create array of letters in translation pairs
+  const translations = input[3].replaceAll(" ", "").split("");
+  // check if there any letters in translation pairs that are not in letter pairs
+  const lettersLeftOver = translations.filter((letter) => !letterPairs.includes(letter) );
+  return lettersLeftOver.length == 0 ? ["yes"] : ["no"] 
 }
 
 module.exports = codeChecker;
--- codeChecker.test.js
+++ codeChecker.test.js
@@ -4,8 +4,14 @@ describe('CodeChecker',() => {
   it('returns yes when a simple code is valid',() => {
     expect(checkCode([1, 1, "ab", "aa bb"])).toEqual(["yes"]) 
   })
+  it('returns yes when a different simple code is valid',() => {
+    expect(checkCode([1, 1, "ac", "aa cc"])).toEqual(["yes"]) 
+  })
   it ('returns no when a simple code is invalid',() => {
     expect(checkCode([1, 1, "ab", "ac ac"])).toEqual(["no"]) 
+  })
+  it ('returns no when a different code is invalid, using letters not contained in the pair',() => {
+    expect(checkCode([1, 1, "ab", "cc dd"])).toEqual(["no"])
   })
 })
 
