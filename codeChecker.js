66d366654630b883842ef09adbb3ad484c24e6ee
Kay <kard@hotmail.co.uk>
2023-03-28 08:22:26 +0100

    Passes the test by checking if any letters in the translation pairs are unchanged

--- codeChecker.js
+++ codeChecker.js
@@ -4,8 +4,15 @@ const codeChecker = (input) => {
   // create array of letters in translation pairs
   const translations = input[3].replaceAll(" ", "").split("");
   // check if there any letters in translation pairs that are not in letter pairs
-  return checkForExtraLetters(translations, letterPairs);
-  
+  if (checkForExtraLetters(translations, letterPairs)[0] == "no") {
+    return ["no"]
+  } else if (translations[0] == translations[2]) { 
+    return ["no"] 
+  } else if (translations[1] == translations[3]) {
+    return ["no"]
+  } else {
+    return ["yes"]
+  }
 }
 
 const checkForExtraLetters = (translations,letterPairs) => {
