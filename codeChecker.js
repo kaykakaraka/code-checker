500834653aa2e24fffbd0ba6a66950085f162556
Kay <kard@hotmail.co.uk>
2023-03-28 08:26:31 +0100

    Refactored out method to check if letters are unchanged

--- codeChecker.js
+++ codeChecker.js
@@ -6,18 +6,24 @@ const codeChecker = (input) => {
   // check if there any letters in translation pairs that are not in letter pairs
   if (checkForExtraLetters(translations, letterPairs)[0] == "no") {
     return ["no"]
-  } else if (translations[0] == translations[2]) { 
-    return ["no"] 
-  } else if (translations[1] == translations[3]) {
-    return ["no"]
   } else {
-    return ["yes"]
+    return areLettersUnchanged(translations) ? ["no"] : ["yes"];
   }
 }
 
 const checkForExtraLetters = (translations,letterPairs) => {
   const lettersLeftOver = translations.filter((letter) => !letterPairs.includes(letter) );
   return lettersLeftOver.length == 0 ? ["yes"] : ["no"] 
+}
+
+const areLettersUnchanged = (translations) => {
+  if (translations[0] == translations[2]) { 
+    return true
+  } else if (translations[1] == translations[3]) {
+    return true
+  } else {
+    return false 
+  }
 }
 
 module.exports = codeChecker;
